b4f63727da6f270389bd17eecf43b3cd1dd162be
fayaz-bahamany <140581005+fayaz-bahamany@users.noreply.github.com>
2023-09-01 15:55:42 +0400

    KAN-1 Updated file

--- src/main/java/com/github/poc/java_project/GetResult.java
+++ src/main/java/com/github/poc/java_project/GetResult.java
@@ -7,7 +7,7 @@ public static int add(int i, int j)
 int sum = 0;
 sum = i + j;
     System.out.println("The sum of the numbers is: "+sum);  
-
+	System.out.println("THis is one more print statement");
 return sum;
   
 }  
